512339a44923c405c7b1309194c67d267f36990d
Dane Johnson <dane_johnson@ymail.com>
2017-01-25 11:28:49 -0600

    Moved http to axios calls for consistency

--- src/Skills.js
+++ src/Skills.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import axios from 'axios'
 import { API_URL } from './Env'
 
 import { centerLayout } from './Styles'
@@ -12,12 +13,10 @@ export default class Skills extends Component {
     }
   }
   componentDidMount() {
-    console.log(API_URL + "/skills/")
-    fetch(API_URL + "/skills/")
-      .then(res => res.json())
+    axios.get(`${API_URL}/skills/`)
       .then(res => {
         this.setState({
-          skills: res.slice()
+          skills: res.data.slice()
         })
       })
       .catch(err => console.error(err));
